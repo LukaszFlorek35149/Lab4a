eb173a95e7f70f089b19ef6568ce74f620304378
lukasz <lukaszflorek22@wp.pl>
2021-05-27 10:44:06 +0200

    Konstruktor Infografika i TestInfografika

---
+++ src/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -0,0 +1,67 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Infografika {
+
+    public String tytul;
+    public String adresStrony;
+    public String adresGrafiki;
+    public String adresMiniaturki;
+    public Integer szerokosc;
+    public Integer wysokosc;
+
+
+    public Infografika(String text)
+    {
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(text);
+        if(m.find())
+        {
+            tytul = m.group(1);
+        }
+        else tytul="";
+
+        pat = Pattern.compile("<link>(.*)<\\/link>");
+        m = pat.matcher(text);
+        if(m.find())
+        {
+            adresStrony = m.group(1);
+        }
+        else adresStrony="";
+
+        pat = Pattern.compile("<media:content url=\"(.*)\" type");
+        m = pat.matcher(text);
+        if(m.find())
+        {
+            adresGrafiki = m.group(1);
+        }
+        else adresGrafiki="";
+
+        pat = Pattern.compile("<media:thumbnail url=\"(.*)\"");
+        m = pat.matcher(text);
+        if(m.find()) {
+            adresMiniaturki = m.group(1);
+        }
+        else
+            adresMiniaturki="";
+
+        pat = Pattern.compile("\" width=\"(.*)\" height=(.*)\">");
+        m = pat.matcher(text);
+        if(m.find())
+        {
+            szerokosc = Integer.parseInt(m.group(1));
+        }
+        else szerokosc = 0;
+
+        pat = Pattern.compile("height=\"(.*)\">");
+        m = pat.matcher(text);
+        if(m.find())
+        {
+            wysokosc = Integer.parseInt(m.group(1));
+        }
+        else wysokosc = 0;
+    }
+
+}
