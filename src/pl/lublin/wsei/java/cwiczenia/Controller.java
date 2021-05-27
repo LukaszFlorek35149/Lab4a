9cb5a6467eecf47771fdb7a3b068d7c1bafe1982
lukasz <lukaszflorek22@wp.pl>
2021-05-27 13:11:21 +0200

    lab 4b Zad 3 – konfiguracja przycisku i labelki plus przetestowanie dotychczasowej pracy

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,4 +1,33 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
 public class Controller {
+    public Label IbFile;
+
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
+
+    @FXML
+    public void initialize()
+    {
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null)
+        {
+            IbFile.setText(file.getAbsolutePath());
+        }
+        else{
+            IbFile.setText("Proszę wczytać plik");
+        }
+    }
 }
