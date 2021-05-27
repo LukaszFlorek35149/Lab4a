65f5b42206c24adb36ab2e6bac2c497539d34077
lukasz <lukaszflorek22@wp.pl>
2021-05-27 13:24:35 +0200

    2.	Zad 4 Wypełnienie listą tytułów infografik

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,17 +1,25 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class Controller {
     public Label IbFile;
+    public ListView lstInfografiki;
 
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)","*.xml");
+
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
 
     @FXML
     public void initialize()
@@ -24,7 +32,10 @@ public class Controller {
         File file = fileChooser.showOpenDialog(null);
         if(file != null)
         {
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
             IbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
         }
         else{
             IbFile.setText("Proszę wczytać plik");
